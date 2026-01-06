6d4f01a3ffa3a15521ace623e2900d9492f6c6cb
Ethan Borrero <ethanb180@nycstudents.net>
2026-01-06 18:05:43 +0000

    asgfa

---
+++ WDLesson3_3Practical/script.js
@@ -0,0 +1,21 @@
+/* Challenge 2: Create a function for each image that displays a description of the picture into the div with an id of highlight*/
+function canada(){
+    document.getElementById("highlight").innerHTML = "Toronto"
+}
+
+function alaska(){
+    document.getElementById("highlight").innerHTML = "Alaska Forest"
+}
+
+function England(){
+    document.getElementById("highlight").innerHTML = "The City Of London"
+}
+
+function NewYork(){
+    document.getElementById("highlight").innerHTML = "Central Park and the City"
+}
+
+function Russia(){
+    document.getElementById("highlight").innerHTML = "Moscow Red Square"
+}
+
