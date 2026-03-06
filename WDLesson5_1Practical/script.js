9ea32b49c4ffd073256461039051b7e3875cd9b4
Ethan Borrero <ethanb180@nycstudents.net>
2026-03-06 17:49:26 +0000

    dsaa

--- WDLesson5_1Practical/script.js
+++ WDLesson5_1Practical/script.js
@@ -14,11 +14,17 @@ Year N: $xxxxxx
 */
 
 function calculate(){
-    let P = parseInt(document.getElementById("p").value);
-    let R = parseInt(document.getElementById("r").value);
+    let P = parseFloat(document.getElementById("p").value);
+    let R = parseFloat(document.getElementById("r").value);
     let N = parseInt(document.getElementById("n").value);
     let yrs = parseInt(document.getElementById("t").value);
     let build = "";
 
-    for(let t = 1; t <= yrs; t++){
+    for(let t = 0; t <= yrs; t++){
+        let A = p * (1 + r/n)**(n*t);
+        build += `year ${t}: $${A.toFixed(2)} <br>`;
+    }
+
+    output.innerHTML = build;
+    
 }
