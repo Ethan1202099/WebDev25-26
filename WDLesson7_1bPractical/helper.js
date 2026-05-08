d249f358c7934b8bca4689c485ebb36903ecf93e
Ethan Borrero <ethanb180@nycstudents.net>
2026-05-08 17:17:34 +0000

    ffxgsdf

---
+++ WDLesson7_1bPractical/helper.js
@@ -0,0 +1,18 @@
+//The function get() below accepts an id of an element and returns the actual element with that id.
+//This function serves to shorten the code previously used to get an element with getElementByID().
+function get(id){
+  return document.getElementById(id);
+  }
+
+  //Challenge 1: Create a function card() that accepts the JSON data for each violation, then generates and returns an appropriate card for the violation.
+  function card(data){
+  return `
+    <div class="card">
+      <h2>${data.violation}</h2>
+      <p>Date ${data.date}</p>
+      <p>Location ${data.location}</p>
+      <p>Amount $${data.amount}</p>
+      <p class = Status ${data.status}</p>
+    </div>
+  `;
+}
